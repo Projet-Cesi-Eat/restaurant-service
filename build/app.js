e9e48be3870c86f45f482e5309f8ae13d5e54b61
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-06-23 14:58:05 +0200

    Initial commit

---
+++ build/app.js
@@ -0,0 +1,23 @@
+"use strict";
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const app = express();
+app.use(logger('dev'));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+module.exports = app;
+//# sourceMappingURL=app.js.map
